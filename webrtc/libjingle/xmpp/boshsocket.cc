191bfa0e595211d714b70002774906bf3979f3ea
David Scherba <david.scherba@personifyinc.com>
2015-09-11 21:48:09 -0500

    CHAT-2370: XMPP BOSH: Fix handling of "pending" sockets
    - When using BOSH transport, implementation would prematurely treat
      sockets as not "pending" operations
         - Depending on timing, this can result in stanzas being
           generated that cannot be immediately sent
         - And this can result in other stanzas being sent on another
           available socket with an out-of-order rid -> connection
           termination

--- webrtc/libjingle/xmpp/boshsocket.cc
+++ webrtc/libjingle/xmpp/boshsocket.cc
@@ -39,36 +39,37 @@ namespace buzz {
     BoshSocket *socket = (BoshSocket *)param;
 
     while (running_) {
-      if (!socket->OutputEmpty())
-      {
+      if (!socket->OutputEmpty()) {
         socket->TrySending();
       }
       else {
         time(&current_);
         auto current_socket = socket->GetCurrentSocket();
         auto other_socket = socket->GetOtherSocket();
-        // If there was things is sent before, and we have had inactivity time.
-        if (inactivity_ != 0 && socket->restart_sent_) {
-          if (current_socket != NULL) {
-            if (current_socket->pending_ == false)
-            {
-              // Send empty request
-              std::string temp = "empty";
-              socket->Write(temp.c_str(), temp.length());
-              socket->TrySending(current_socket);
-            }
-          }
-          if (other_socket != NULL) {
-            if (difftime(current_, other_socket->receive_time_) >= inactivity_ / 2) {
-              if (other_socket->pending_ == false) {
+
+        if (current_socket && current_socket->pending_ == false && other_socket && other_socket->pending_ == false) {
+          // If there was things is sent before, and we have had inactivity time.
+          if (inactivity_ != 0 && socket->restart_sent_) {
+            if (current_socket != NULL) {
+              if (current_socket->pending_ == false) {
                 // Send empty request
                 std::string temp = "empty";
                 socket->Write(temp.c_str(), temp.length());
-                socket->TrySending(other_socket);
+                socket->TrySending(current_socket);
               }
             }
+            if (other_socket != NULL) {
+              if (difftime(current_, other_socket->receive_time_) >= inactivity_ / 2) {
+                if (other_socket->pending_ == false) {
+                  // Send empty request
+                  std::string temp = "empty";
+                  socket->Write(temp.c_str(), temp.length());
+                  socket->TrySending(other_socket);
+                }
+              }
+            }
+            Sleep(50);
           }
-          Sleep(50);
         }
       }
     }
@@ -81,8 +82,7 @@ namespace buzz {
     ts.tv_sec = 0;
     ts.tv_nsec = 50 * kMilliToNano;
     while (running_) {
-      if (!socket->OutputEmpty())
-      {
+      if (!socket->OutputEmpty()) {
         socket->TrySending();
       }
       else {
@@ -259,8 +259,8 @@ namespace buzz {
           std::memcpy(input_msg.data_, error.c_str(), error.length());
           input_msg.recv_result_ = true;
         }
-        temp_socket->pending_ = false;
         if (input_msg.len_ != 0) {
+          temp_socket->pending_ = false;
           input_queue_.push(input_msg);
           SignalRead();
         }
@@ -565,14 +565,12 @@ namespace buzz {
 
   void BoshSocket::TrySending() {
     if (BothConnected()) {
-      if (!BothPending()) {
-        auto temp_socket = GetSocket();
+      auto temp_socket = GetSocket();
 #ifndef USE_SSLSTREAM
-        OnWriteEvent(temp_socket->socket_);
+      OnWriteEvent(temp_socket->socket_);
 #else // USE_SSLSTREAM
-        OnEvent(temp_socket->stream_, rtc::SE_WRITE, 0);
+      OnEvent(temp_socket->stream_, rtc::SE_WRITE, 0);
 #endif // USE_SSLSTREAM
-      }
     }
   }
 
