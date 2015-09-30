3496e0548b5a486820f3bb75e9b7a1b808c5a9c2
digitalmouse12 <glenn.francisco@personifyinc.com>
2015-09-30 16:10:29 -0500

    CHROM-284: Add VisualOn Encoder wrapper

---
+++ webrtc/modules/audio_coding/codecs/aac/include/AacVisualOnEncoder.h
@@ -0,0 +1,36 @@
+#ifndef AAC_VISUAL_ON_ENCODER_H
+#define AAC_VISUAL_ON_ENCODER_H
+
+#include "webrtc/modules/audio_coding/codecs/aac/vo-aacenc/common/include/cmnMemory.h"
+#include "webrtc/modules/audio_coding/codecs/aac/vo-aacenc/common/include/voAAC.h"
+#include "webrtc/typedefs.h"
+
+class AacVisualOnEncoder
+{
+
+public:
+    AacVisualOnEncoder();
+
+    ~AacVisualOnEncoder();
+
+    bool Init();
+
+    int Encode(short* audioIn,
+               int inLen,
+               uint8_t* audioOut);
+
+    void CleanUp();
+
+    unsigned char* getSpecificInfo();
+
+    unsigned long  getSpecificInfoLen();
+
+protected:
+    VO_AUDIO_CODECAPI mCodecApi;
+    VO_HANDLE mHandle;
+    VO_MEM_OPERATOR mMemOperator;
+    VO_CODEC_INIT_USERDATA mUserData;
+    uint8_t* mOutput;
+};
+
+#endif
